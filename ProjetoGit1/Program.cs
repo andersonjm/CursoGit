5048d4c6625d31b14c0b124c3582cf561fded5ee
Anderson Mauricio <andersonjm_2005@hotmail.com>
2020-04-28 15:29:02 -0300

    Acrescentada a mensagem sobre como voce esta hoje

--- ProjetoGit1/Program.cs
+++ ProjetoGit1/Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoGit1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
             Console.ReadLine();
         }
     }
