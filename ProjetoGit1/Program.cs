8ff60a5a3931d6d7eef40f63034c9bbdc98a3cc7
Anderson Mauricio <andersonjm_2005@hotmail.com>
2020-04-28 15:06:16 -0300

    Acrescentada uma mensagem de bom dia

--- ProjetoGit1/Program.cs
+++ ProjetoGit1/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoGit1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
         }
     }
